99c97460ea1ec090be062c4d065f3ef70a967745
Ajay Kumar Reddy Krishnareddy Gari <ajaykumarreddykrishnareddygari@gmail.com>
2026-04-19 01:49:17 +0530

    just loooking
    Implement factorial function to calculate the factorial of a number.

---
+++ Practice.py
@@ -0,0 +1,14 @@
+===============================================
+# Day - 01
+===========================================
+Factorial
+def factorial(n):
+  fact = 1 # intial - > 1
+  for i in range(1,n+1):
+    fact *= i
+  return fact
+if __name__ == "__main__":
+  print("Factorial:,",factorial(2))
+-------------------------------------------------
+Answer will be there<fs
+-------------------------------------------------
